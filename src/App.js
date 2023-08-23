1ed8d9ef24108375ea8ba9e024177a451d3fa65a
Jeffrey Kam <jeffreykam0415@gmail.com>
2023-08-23 13:59:17 -0400

    Updating UI and validation logic

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ function App() {
   const [comment, setComment] = useState('');
   const [datesAway, setDatesAway] = useState([]);
   const [loadedStateCount, setLoadedStateCount] = useState(0);
+  const [validationMessage, setValidationMessage] = useState('');
+  const [validationMessageForDatesAdded, setValidationMessageForDatesAdded] = useState('');
 
   useEffect(() => {
       console.log("Loading data from local storage.");
@@ -56,9 +58,11 @@ function App() {
     }
   }, [applicationDate]);
 
+
+  let initialized = loadedStateCount >= 4;
   // Save on fields update, but not updating until we're fully initialized.
   useEffect(() => {
-    if (loadedStateCount < 4) {
+    if (!initialized) {
       console.log("Progress not saved due to race condition.");
     } else {
       const saveProgress = () => {
@@ -70,13 +74,70 @@ function App() {
       };
       saveProgress();
     }
-  }, [loadedStateCount, datesAway, prDate, wasNpr, applicationDate]);
+  }, [initialized, datesAway, prDate, wasNpr, applicationDate]);
+
+  useEffect(() => {
+    if (!initialized) {
+      console.log("Progress not saved due to race condition.");
+    } else {
+      const validateApplicationDates = () => {
+        if (new Date(prDate) >= new Date(applicationDate)) {
+            setValidationMessage('PR date has to be before the planned application date.');
+            return false;
+        }
+        setValidationMessage('');
+        return true;
+      }
+      validateApplicationDates();
+    }
+  }, [initialized, prDate, applicationDate]);
 
   const addDatesAway = () => {
-    setDatesAway([...datesAway, { start: startDate, end: endDate, comment: comment }]);
-    setStartDate('');
-    setEndDate('');
-    setComment('');
+    const isValidStartDate = (startDate, endDate) => {
+      return new Date(startDate) < new Date(endDate);
+    };
+    const isValidCommentLength = (comment) => {
+      return comment.length <= 200;
+    };
+    const hasNoOverlappingDates = (startDate, endDate, existingDates) => {
+      const start = new Date(startDate);
+      const end = new Date(endDate);
+      for (let range of existingDates) {
+          const rangeStart = new Date(range.start);
+          const rangeEnd = new Date(range.end);
+          if ((start >= rangeStart && start <= rangeEnd) ||
+              (end >= rangeStart && end <= rangeEnd) ||
+              (start <= rangeStart && end >= rangeEnd)) {
+              return false;
+          }
+      }
+      return true;
+    };
+    const validateDateAway = () => {
+      if (!isValidStartDate(startDate, endDate)) {
+        setValidationMessageForDatesAdded('The start date has to be before the end date.');
+        return false;
+      }
+      if (!isValidCommentLength(comment)) {
+        setValidationMessageForDatesAdded('The maximum comment length is 200. Currently at ' + comment.length + '.');
+        return false;
+      }
+      if (!hasNoOverlappingDates(startDate, endDate, datesAway)) {
+        setValidationMessageForDatesAdded('The date range being added overlaps with some of the existing dates.');
+        return false;
+      }
+      setValidationMessageForDatesAdded('');
+      return true;
+    }
+    let result = validateDateAway();
+    if (!result) {
+      console.log("Will not add data until the validation passes.");
+    } else {
+      setDatesAway([...datesAway, { start: startDate, end: endDate, comment: comment }]);
+      setStartDate('');
+      setEndDate('');
+      setComment('');
+    }
   };
 
   const removeDate = (index) => {
@@ -102,7 +163,7 @@ function App() {
 
   datesAway.forEach(d => {
     let from = new Date(d.start);
-    const to = new Date(d.end);
+    let to = new Date(d.end);
 
     // If the entire duration of absence is before startCountDate, skip
     if (to < startCountDate) {
@@ -113,14 +174,33 @@ function App() {
     if (from < startCountDate) {
       from = startCountDate;
     }
-    const daysDifference = (to - from) / (DAYS_IN_MILLISECONDS) - 1 >= 0 ? (to - from) / (DAYS_IN_MILLISECONDS) - 1 : 0;
 
+    let daysDifference = 0;
     if (from < parsedPrDate) {
       // Before PR
-      totalDaysAwayBeforePR += daysDifference;
+      if (to >= parsedPrDate) {
+        // Split between before and after pr date
+        let toBeforePR = parsedPrDate;
+        let fromAfterPR = parsedPrDate;
+        let daysDifferenceBeforePR = (toBeforePR - from) / (DAYS_IN_MILLISECONDS) - 1 >= 0 ? (to - from) / (DAYS_IN_MILLISECONDS) - 1 : 0;
+        let daysDifferenceAfterPR = (to - fromAfterPR) / (DAYS_IN_MILLISECONDS) - 1 >= 0 ? (to - from) / (DAYS_IN_MILLISECONDS) - 1 : 0;
+        totalDaysAwayBeforePR += daysDifferenceBeforePR + 1;
+        totalDaysAwayAfterPR += daysDifferenceAfterPR + 1;
+      } else {
+        daysDifference = (to - from) / (DAYS_IN_MILLISECONDS) - 1 >= 0 ? (to - from) / (DAYS_IN_MILLISECONDS) - 1 : 0;
+        totalDaysAwayBeforePR += daysDifference;
+      }
     } else {
       // After PR
-      totalDaysAwayAfterPR += daysDifference;
+      if (to >= applicationDate) {
+        // Cap at application date
+        to = applicationDate;
+        daysDifference = (to - from) / (DAYS_IN_MILLISECONDS) - 1 >= 0 ? (to - from) / (DAYS_IN_MILLISECONDS) - 1 : 0;
+        totalDaysAwayAfterPR += daysDifference;
+      } else {
+        daysDifference = (to - from) / (DAYS_IN_MILLISECONDS) - 1 >= 0 ? (to - from) / (DAYS_IN_MILLISECONDS) - 1 : 0;
+        totalDaysAwayAfterPR += daysDifference;
+      }
     }
   });
 
@@ -130,18 +210,18 @@ function App() {
   } else {
     totalDaysAwayBeforePR = 0;
   }
-  console.log("Maximum possible days in Canada before PR within the 5 years timeframe (excluding 365 days limit):", maxTotalDaysHereBeforePR);
-  console.log("Total days away before PR (excluding 365 days limit):", totalDaysAwayBeforePR);
-  console.log("Total days counted before PR (capped at 365 days):", totalDaysHereBeforePR);
+  // console.log("Maximum possible days in Canada before PR within the 5 years timeframe (excluding 365 days limit):", maxTotalDaysHereBeforePR);
+  // console.log("Total days away before PR (excluding 365 days limit):", totalDaysAwayBeforePR);
+  // console.log("Total days counted before PR (capped at 365 days):", totalDaysHereBeforePR);
 
   let maxTotalDaysHereAfterPR = (parsedApplicationDate - parsedPrDate) / DAYS_IN_MILLISECONDS;
   totalDaysHereAfterPR = maxTotalDaysHereAfterPR - totalDaysAwayAfterPR;
-  console.log("Maximum possible days in Canada after PR within the 5 years timeframe:", maxTotalDaysHereAfterPR);
-  console.log("Total days away before PR:", totalDaysAwayAfterPR);
-  console.log("Total days counted before PR:", totalDaysHereAfterPR);
+  // console.log("Maximum possible days in Canada after PR within the 5 years timeframe:", maxTotalDaysHereAfterPR);
+  // console.log("Total days away before PR:", totalDaysAwayAfterPR);
+  // console.log("Total days counted before PR:", totalDaysHereAfterPR);
 
   const totalDaysInCanada = totalDaysHereBeforePR + totalDaysHereAfterPR;
-  console.log("Total days residing in Canada (for the purpose of immigration):", totalDaysInCanada);
+  // console.log("Total days residing in Canada (for the purpose of immigration):", totalDaysInCanada);
 
   return (
     <div className="App">
@@ -153,15 +233,17 @@ function App() {
       </div>
 
       <div className="input-section">
+        <label>Planned Application Date: </label>
+        <input type="date" value={applicationDate} onChange={(e) => setApplicationDate(e.target.value)} />
+      </div>
+
+      {validationMessage && <p style={{color: 'red'}}>{validationMessage}</p>}
+
+      <div className="input-section">
         <label>
           <input type="checkbox" checked={wasNpr} onChange={(e) => setWasNpr(e.target.checked)} />
           Were you a legal temporary resident of Canada before PR?
         </label>
-      </div>
-
-      <div className="input-section">
-        <label>Planned Application Date: </label>
-        <input type="date" value={applicationDate} onChange={(e) => setApplicationDate(e.target.value)} />
       </div>
 
       <div className="input-section">
@@ -172,27 +254,43 @@ function App() {
         <label>Reason:</label>
         <input type="text" placeholder="Travel" value={comment} onChange={(e) => setComment(e.target.value)} />
       </div>
+      {validationMessageForDatesAdded && <p style={{color: 'red'}}>{validationMessageForDatesAdded}</p>}
 
       <div className="input-section">
         <button onClick={addDatesAway}>Add</button>
       </div>
 
       <ul>
-        {datesAway.map((date, index) => (
-          <li key={index}>
-            {date.start} - {date.end} ({date.comment})
-            {' '}
-            <button onClick={() => removeDate(index)}>Remove</button>
-          </li>
-        ))}
+        {datesAway
+          .sort((a, b) => new Date(a.start) - new Date(b.start))
+          .map((dateRange, index) => (
+            <li key={index} className="list-item">
+              <span className="date-column">{dateRange.start} to {dateRange.end}</span>
+              <span className="comment-column">{dateRange.comment}</span>
+              <button className="remove-button-column" onClick={() => removeDate(index)}>Remove</button>
+            </li>
+          ))}
       </ul>
 
       <h2>Total days in Canada (within the 5 year period): {totalDaysInCanada}</h2>
 
-      Note: The "Left Canada" and "Enter Canada" time is inclusive; we count those two days as part of being inside Canada.
-      {/* <div className="input-section">
-        <button onClick={saveProgress}>Save Progress</button>
-      </div> */}
+      <div>
+        Note:
+        <ul>
+          <li>
+            The "Left Canada" and "Enter Canada" dates are inclusive. Those two days are counted as being in Canada.
+          </li>
+          <li>
+            The app autosaves everytime an input is changed, but it only starts autosaving after all the fields are filled.
+          </li>
+          <li>
+            Data are only stored locally in localStroage.
+          </li>
+          <li>
+            This is only meant to be a useful utility. Please still confirm all the residency calculations on your own.
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
